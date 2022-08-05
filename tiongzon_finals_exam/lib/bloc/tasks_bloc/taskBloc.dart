6fc7fb9f8c6c736c4db2ed488f33c041e38bcf4d
Pete Tiongzon <pete.tiongzon@gmail.com>
2022-08-06 01:00:02 +0800

    Edit and Remove. Starting to implement Bloc

--- tiongzon_finals_exam/lib/bloc/tasks_bloc/taskBloc.dart
+++ tiongzon_finals_exam/lib/bloc/tasks_bloc/taskBloc.dart
@@ -8,6 +8,9 @@ import 'taskState.dart';
 class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
   TasksBloc() : super(const TasksState()) {
     on<AddTask>(_onAddTask);
+    on<UpdateTask>(_onUpdateTask);
+    on<DeleteTask>(_onDeleteTask);
+    on<RemoveTask>(_onRemoveTask);
   }
 
   void _onAddTask(AddTask event, Emitter<TasksState> emit) {
@@ -19,4 +22,67 @@ class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
       removedTasks: state.removedTasks,
     ));
   }
+}
+
+void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
+  final state = this.state;
+  final task = event.task;
+
+  List<Task> pendingTasks = state.pendingTasks;
+  List<Task> completedTasks = state.completedTasks;
+  List<Task> favoriteTasks = state.favoriteTasks;
+  if (task.isDone == false) {
+    if (task.isFavorite == false) {
+      pendingTasks = List.from(pendingTasks)..remove(task);
+      completedTasks.insert(0, task.copyWith(isDone: true));
+    } else {
+      var taskIndex = favoriteTasks.indexOf(task);
+      pendingTasks = List.from(pendingTasks)..remove(task);
+      completedTasks.insert(0, task.copyWith(isDone: true));
+      favoriteTasks = List.from(favoriteTasks)
+        ..remove(task)
+        ..insert(taskIndex, task.copyWith(isDone: true));
+    }
+  } else {
+    if (task.isFavorite == false) {
+      completedTasks = List.from(completedTasks)..remove(task);
+      pendingTasks = List.from(pendingTasks)
+        ..insert(0, task.copyWith(isDone: false));
+    } else {
+      var taskIndex = favoriteTasks.indexOf(task);
+      completedTasks = List.from(completedTasks)..remove(task);
+      pendingTasks = List.from(pendingTasks)
+        ..insert(0, task.copyWith(isDone: false));
+      favoriteTasks = List.from(favoriteTasks)
+        ..remove(task)
+        ..insert(taskIndex, task.copyWith(isDone: false));
+    }
+  }
+  emit(TasksState(
+    pendingTasks: pendingTasks,
+    completedTasks: completedTasks,
+    favoriteTasks: state.favoriteTasks,
+    removedTasks: state.removedTasks,
+  ));
+}
+
+void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {
+  final state = this.state;
+  emit(TasksState(
+    pendingTasks: state.pendingTasks,
+    completedTasks: state.completedTasks,
+    favoriteTasks: state.favoriteTasks,
+    removedTasks: List.from(state.removedTasks)..remove(event.task),
+  ));
+}
+
+void _onRemoveTask(RemoveTask event, Emitter<TasksState> emit) {
+  final state = this.state;
+  emit(TasksState(
+    pendingTasks: List.from(state.pendingTasks)..remove(event.task),
+    completedTasks: List.from(state.completedTasks)..remove(event.task),
+    favoriteTasks: List.from(state.favoriteTasks)..remove(event.task),
+    removedTasks: List.from(state.removedTasks)
+      ..add(event.task.copyWith(isDeleted: true)),
+  ));
 }
--- tiongzon_finals_exam/lib/main.dart
+++ tiongzon_finals_exam/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app_router.dart';
 import 'app_themes.dart';
+import 'blocs/bloc_exports.dart';
 import 'screens/tabs_screen.dart';
 
-void main() {
-  runApp(
-    MyApp(appRouter: AppRouter()),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+
+  HydratedBlocOverrides.runZoned(
+    () => runApp(MyApp(appRouter: AppRouter())),
+    storage: storage,
   );
 }
 
@@ -17,11 +26,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BloC Tasks App',
-      theme: AppThemes.appThemeData[AppTheme.lightMode],
-      home: const TabsScreen(),
-      onGenerateRoute: appRouter.onGenerateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => TasksBloc(),
+        ),
+        BlocProvider(
+          create: (context) => SwitchBloc(),
+        ),
+      ],
+      child: BlocBuilder<SwitchBloc, SwitchState>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'BloC Tasks App',
+            theme: state.switchValue
+                ? AppThemes.appThemeData[AppTheme.darkMode]
+                : AppThemes.appThemeData[AppTheme.lightMode],
+            home: const TabsScreen(),
+            onGenerateRoute: appRouter.onGenerateRoute,
+          );
+        },
+      ),
     );
   }
 }
