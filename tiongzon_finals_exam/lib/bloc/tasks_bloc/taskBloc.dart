f8507dce0208ff40f915168d95fbdd4c71aa8c5a
Pete Tiongzon <pete.tiongzon@gmail.com>
2022-08-06 00:33:27 +0800

    Add Task

---
+++ tiongzon_finals_exam/lib/bloc/tasks_bloc/taskBloc.dart
@@ -0,0 +1,22 @@
+import 'package:bloc_finals_exam/bloc/blocExports.dart';
+import 'package:bloc_finals_exam/models/task.dart';
+import 'package:equatable/equatable.dart';
+
+import 'taskEvent.dart';
+import 'taskState.dart';
+
+class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
+  TasksBloc() : super(const TasksState()) {
+    on<AddTask>(_onAddTask);
+  }
+
+  void _onAddTask(AddTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(
+      pendingTasks: List.from(state.pendingTasks)..add(event.task),
+      completedTasks: state.completedTasks,
+      favoriteTasks: state.favoriteTasks,
+      removedTasks: state.removedTasks,
+    ));
+  }
+}
--- tiongzon_finals_exam/lib/widgets/add_edit_task.dart
+++ tiongzon_finals_exam/lib/widgets/add_edit_task.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_finals_exam/bloc/tasks_bloc/taskBloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../models/task.dart';
 
@@ -90,6 +92,19 @@ class _AddEditTaskState extends State<AddEditTask> {
                 ElevatedButton(
                   onPressed: _title.isNotEmpty && _description.isNotEmpty
                       ? () {
+                          final task = Task(
+                              id: widget.task != null ? widget.task!.id : null,
+                              title: _title,
+                              description: _description,
+                              isFavorite: widget.task != null
+                                  ? widget.task!.isFavorite!
+                                  : null,
+                              isDone: widget.task != null
+                                  ? widget.task!.isDone!
+                                  : null);
+                          context
+                              .read<TaskBloc>()
+                              .add(CreateEditTask(task: task));
                           Navigator.pop(context);
                         }
                       : null,
